755120c5bf394321bac0c25590108089d573d0ac
Arjun V K <vkarjun.8848@gmail.com>
2025-01-05 21:55:22 +0530

    add redux toolkit

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react';
 import { Link,useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { signInStart, signInSuccess, signInFailure } from '../redux/user/userSlice';
 
 export default function SignIn() {
   const [formData, setFormData] = useState({})
-  const [error, setError] = useState(null)
-  const [loading, setLoading] = useState(false) 
+  const {loading, error} = useSelector((state) => state.user);
   const navigate = useNavigate();
+  const dispatch = useDispatch();
   
   const handleChange = (e) => {
     setFormData({
@@ -22,7 +24,7 @@ export default function SignIn() {
     }
 
     try {
-      setLoading(true);
+      dispatch(signInStart());
       const res = await fetch('/api/auth/signin',
       {
         method: 'POST',
@@ -33,16 +35,13 @@ export default function SignIn() {
       });
       const data = await res.json();
       if(data.success === false) {
-        setError('Username or email already exists.');
-        setLoading(false);
+        dispatch(signInFailure(data.message));
         return;
       }
-      setLoading(false);
-      setError(null);
+      dispatch(signInSuccess(data));
       navigate('/');
     } catch (error) {
-      setLoading(false);
-      setError(error.message);
+      dispatch(signInFailure(error.message));
     }
     
     console.log(data);
